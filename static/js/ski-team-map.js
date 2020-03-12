31b642bc7a06e899c96c45d545ce159d50b275ba
cesurunsur <cesurunsur@gmail.com>
2020-03-12 10:43:11 -0400

    json question

---
+++ static/js/ski-team-map.js
@@ -0,0 +1,27 @@
+var myMap = L.map("map", {
+  center: [39.381266, -97.922211],
+  zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var newtry = "/ski-team";
+
+
+d3.json(newtry, function(data) {
+
+  console.log(data);
+
+  for (var i = 0; i < data.length; i++) {
+   // var location = response[i].location;
+    L.marker([data[i].lat, data[i].long])
+    .bindPopup("<h1>" + data[i].name + "</h1> <hr> <h3>" + data[i].team + "</h1> <hr> <h3>" + data[i].city + "</h3>").addTo(myMap)
+  }
+
+});
